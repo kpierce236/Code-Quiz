aca5b2746f5f90c61752f1ac97da8bd1629cf1dc
Kevin Pierce <kpierce236@gmail.com>
2023-10-30 19:46:39 -0400

    added comments to js file

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
-//Start Timer
+//Selected Elements for manipulation
 var timeEl = document.querySelector(".timer");
 var startButton = document.querySelector(".startbtn");
 var containerEl= document.querySelector(".container");
 var viewScoresEl= document.querySelector(".high-scores");
 
+//Timer seconds variable
 var secondsLeft = 75;
 
+//Function that sets timer for quiz
 function setTime() {
-    // Sets interval in variable
     var timerInterval = setInterval(function() {
       timeEl.textContent = "Time: " + secondsLeft;
       secondsLeft--;
@@ -22,11 +23,11 @@ function setTime() {
     }, 1000);
   }
 
+//Event listners that run seperate functions for the start quiz buttpn
 startButton.addEventListener("click", setTime);
 startButton.addEventListener("click", init);
 
-//array of questions, selections, and anwsers
-
+//Arrays of questions, selections, and anwsers
 var questions = ["Commonly used data types DO NOT include:","The condtions in an if / else statement is enclosed within ____.", "Arrays in javaScript can be used to store ____.","String values must be enclosed within _____ when being assigned to variables.","A very useful tool used during development and debugging for printing content to the debugger is:"];
 var selectionObject = {
     q0 : ["strings", "booleans", "alerts", "numbers"],
@@ -38,23 +39,35 @@ var selectionObject = {
 
 var answers = ["alerts","parentheses","all of the above","quotes","console.log"];
 
+//Position variable that sets directs the program to which set of question, selections, and answer we are on
 var position = 0;
 
+//Holds the current score of the quiz
 var score = 0;
 
+//Selects and stores the title, and subject element of the page
 var titleEl = document.querySelector(".title");
 var subjectEl = document.querySelector(".subject");
+
+//Creates a list element to store the selections for the question in
 var list = document.createElement("ol");
 
-//Post inital question when start button is clicked
+//Posts inital question when start button is clicked
 function init() {
+    //Removes start button
     startButton.remove();
+
+    //Changes the class of the title and subject elements
     titleEl.setAttribute("class","question");
     subjectEl.setAttribute("class","answer");
+
+    //Replaces the subject element with the created list element declared outside the function
     subjectEl.replaceWith(list);
 
+    //Sets the title element equal to the first question
     titleEl.textContent = questions[position];
     
+    //For loop that iterates over the selections for the given question and then creates list elements
     for (var i = 0; i < selectionObject.q0.length; i++) {
        var answerBtn = document.createElement("li");
        list.appendChild(answerBtn);
@@ -67,7 +80,6 @@ function init() {
     
 }
 // Creates the next question when you answer the previous one
-
 function nextQuestion() {
    position += 1;
    
@@ -94,8 +106,7 @@ function nextQuestion() {
  };
 
 
-//function checks anwser of clicked selection
-
+//Function checks anwser of clicked selection and then brings you the next question
 function checkAnswer (event) {
     if (event.target.textContent === answers[position]) {
         correctAnswer();
@@ -118,7 +129,7 @@ function wrongAnswer () {
     secondsLeft = secondsLeft - 10;
 
     
-    wrongSeconds = 2;
+    wrongSeconds = 1;
     var timer = setInterval(function() {
         list.append(wrong);
         wrongSeconds--;
@@ -135,6 +146,9 @@ function wrongAnswer () {
 
 }
 
+// Function that runs when answer selected is correct
+// Displays message and addes ten points to your score
+
 function correctAnswer () {
     score += 10;
     var correct = document.createElement("div");
@@ -142,7 +156,7 @@ function correctAnswer () {
     correct.setAttribute("class","response");
     
 
-    correctSeconds = 2;
+    correctSeconds = 1;
     var timerTwo = setInterval(function() {
         list.append(correct);
         correctSeconds--;
@@ -158,15 +172,23 @@ function correctAnswer () {
 
 
 }
-
+//This function is run when either time runs out or you answer all questions
+//This functions creates an HTMl that has a input field where you submit your intial with the given score.
 function endGame() {
+    //resets seconds to zero
     secondsLeft = 0;
 
+    //Changes title
     titleEl.textContent = "All done!"
+
+    //Adds statement with your final score stated
     var statementEl = document.createElement("p");
     statementEl.setAttribute("style","text-align:left;")
     statementEl.textContent = "Your final score is " + score + ".";
+    //replaces list element with statement
     list.replaceWith(statementEl);
+
+    //Appends HTML to the statement element that includes an input field and submit button
     var submitEl = '<span> Enter initials: <input class="input"></input><button class="submit">Submit</button>';
     var submitContainerEl = document.createElement("div");
     statementEl.append(submitContainerEl);
@@ -177,6 +199,8 @@ function endGame() {
     var submitButton = document.querySelector(".submit");
     var intialEl = document.querySelector(".input");
 
+    //Event listener for the newly created submit button that posts the given score and intial to local storage
+    // After storing the intial and score the displayHighScores function is run
     submitButton.addEventListener ("click", function (event) {
         event.preventDefault();
         var intial = intialEl.value;
@@ -187,15 +211,24 @@ function endGame() {
     });
     
 }
+
+// Runs the displayHighScores function when the element that contains View highscores text is clicked
 viewScoresEl.addEventListener("click",displayHighScores);
 
+//Renders new HTML that displays the highscores stored in localstorage
+
 function displayHighScores() {
+    //resets timer to zero
     secondsLeft = 0;
+
+    //Creates highscores title and an ordered list to store the scores
     containerEl.innerHTML = '<h1 class="highscore">Highscores</h1>';
     var highscoreTitle = document.querySelector(".highscore");
     var scoreListEl = document.createElement("ol");
     highscoreTitle.append(scoreListEl);
 
+    //for loop that goes through local storage and then posts a list element to append the score list element 
+    //that contains the highscore with intial
     for (var i = 0; i < localStorage.length; i++) {
         var key = localStorage.key(i);
         var value = localStorage[key];
@@ -209,18 +242,21 @@ function displayHighScores() {
         
      };
 
+     //creates container that holds the two curated buttons for the display highscores page
     var btnContainer = document.createElement("div");
     scoreListEl.append(btnContainer);
     btnContainer.innerHTML = "<button class='back'>Go Back</button> <button class='clear'>Clear Highscores</button>";
 
     var clearBtn = document.querySelector(".clear");
     var backBtn = document.querySelector(".back");
-
+    
+    //when clear highscores is clicked then local sotrage is cleared
     clearBtn.addEventListener("click", function () {
         localStorage.clear();
         //scoreListEl.innerHTML = "";
     })
 
+    //when the back button is clicked then the browser reloads to the startpage
     backBtn.addEventListener("click", function () {
        location.reload();
 
