dd9238fd12e12986f345f29ab6c6d904085930d6
Kevin Pierce <kpierce236@gmail.com>
2023-10-30 11:36:07 -0400

    added some css and js for addQuestions function

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,8 @@ function setTime() {
   }
 
 startButton.addEventListener("click", setTime);
+startButton.addEventListener("click", addQuestions);
+//array of questions, selections, and anwsers
 
 var questions = ["Commonly used data types DO NOT include:","The condtions in an if / else statement is enclosed within ____.", "Arrays in javaScript can be used to store ____.","String values must be enclosed within _____ when being assigned to variables.","A very useful tool used during development and debugging for printing content to the debugger is:"];
 
@@ -30,4 +32,25 @@ var q4 = ["commas","curly brackets","quotes","parenthese"];
 var q5 = ["JavaScript","terminal/bash","for loops","console.log"];
 
 var answers = ["alerts","parenthese","all of the above","quotes","console.log"];
+
+var titleEl = document.querySelector(".title");
+var subjectEl = document.querySelector(".subject");
+
 //Make questions
+function addQuestions() {
+    startButton.remove();
+    titleEl.setAttribute("class","question");
+    subjectEl.setAttribute("class","answer");
+    var list = document.createElement("ol");
+    subjectEl.replaceWith(list);
+
+    titleEl.textContent = questions[0];
+    
+    for (var i = 0; i < q1.length; i++) {
+       var answerBtn = document.createElement("li");
+       list.appendChild(answerBtn);
+       answerBtn.setAttribute("class","answer-btn");
+       answerBtn.textContent= q1[i];
+    };
+    
+}
