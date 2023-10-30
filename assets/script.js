f3c71aefede6b926ed0d535ddd5f21fe2caa5c58
Kevin Pierce <kpierce236@gmail.com>
2023-10-30 10:44:11 -0400

    added arrays containing questions and anwsers for game

--- assets/script.js
+++ assets/script.js
@@ -20,3 +20,14 @@ function setTime() {
   }
 
 startButton.addEventListener("click", setTime);
+
+var questions = ["Commonly used data types DO NOT include:","The condtions in an if / else statement is enclosed within ____.", "Arrays in javaScript can be used to store ____.","String values must be enclosed within _____ when being assigned to variables.","A very useful tool used during development and debugging for printing content to the debugger is:"];
+
+var q1 = ["strings", "booleans", "alerts", "numbers"];
+var q2 = ["quotes", "curly brackets", "parentheses", "square brackets"];
+var q3 = ["numbers and strings","other arrays","booleans","all of the above"];
+var q4 = ["commas","curly brackets","quotes","parenthese"];
+var q5 = ["JavaScript","terminal/bash","for loops","console.log"];
+
+var answers = ["alerts","parenthese","all of the above","quotes","console.log"];
+//Make questions
